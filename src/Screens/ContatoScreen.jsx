29a88ed5f211f3c4177e2f02c3e688cb913e6b62
enzoluizb <enzoluiz.brt@gmail.com>
2022-11-03 15:44:53 -0300

    add divider

--- src/Screens/ContatoScreen.jsx
+++ src/Screens/ContatoScreen.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Text, View } from "react-native"
-import { TextInput } from "react-native-paper"
+import { Divider, TextInput } from "react-native-paper"
 
 export const ContatoScreen = () => {
     const [text, setText] = useState("");
@@ -8,10 +8,20 @@ export const ContatoScreen = () => {
         <View>
             <Text>Página de Contato</Text>
             <TextInput
-      label="Email"
-      value={text}
-      onChangeText={(text) => setText(text)}
-    />
+                label="Email"
+                value={text}
+                onChangeText={(text) => setText(text)}
+            />
+            <TextInput
+                label="Mensagem"
+                value={text}
+                onChangeText={(text) => setText(text)}
+            />
+            <Text>Diretrizes</Text>
+    <Divider />
+    <Text>Ajuda</Text>
+    <Divider />
         </View>
-    )
+    );
 }
+
