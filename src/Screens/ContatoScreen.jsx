8a22bd88aa41753006d0103675a8c7faf248f0d1
enzoluizb <enzoluiz.brt@gmail.com>
2022-11-03 16:31:29 -0300

    Add SearchBar % RadioButton

--- src/Screens/ContatoScreen.jsx
+++ src/Screens/ContatoScreen.jsx
@@ -1,12 +1,22 @@
 import { useState } from "react";
-import { Text, View } from "react-native"
-import { Divider, TextInput } from "react-native-paper"
+import { StyleSheet, Text, View } from "react-native"
+import { Divider, RadioButton, Searchbar, Switch, TextInput } from "react-native-paper"
 
 export const ContatoScreen = () => {
     const [text, setText] = useState("");
+    const [searchQuery, setSearchQuery] = useState('');
+    const onChangeSearch = query => setSearchQuery(query);
+    const [checked, setChecked] = useState('first');
+    const [isSwitchOn, setIsSwitchOn] = useState(false);
+    const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn);
     return (
         <View>
             <Text>Página de Contato</Text>
+            <Searchbar
+                placeholder="Search"
+                onChangeText={onChangeSearch}
+                value={searchQuery}
+            />
             <TextInput
                 label="Email"
                 value={text}
@@ -17,11 +27,32 @@ export const ContatoScreen = () => {
                 value={text}
                 onChangeText={(text) => setText(text)}
             />
+            <RadioButton
+                value="first"
+                status={checked === 'first' ? 'checked' : 'unchecked'}
+                onPress={() => setChecked('first')}
+            />
+            <RadioButton
+                value="second"
+                status={checked === 'second' ? 'checked' : 'unchecked'}
+                onPress={() => setChecked('second')}
+            />
+            <Switch value={isSwitchOn} onValueChange={onToggleSwitch} />
             <Text>Diretrizes</Text>
-    <Divider />
-    <Text>Ajuda</Text>
-    <Divider />
+            <Divider />
+            <Text>Ajuda</Text>
+            <Divider />
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    surface: {
+        padding: 8,
+        height: 80,
+        width: 80,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
 
